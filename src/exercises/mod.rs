748dbbbbe86e78e0de6f41ccaadf32d6fa610762
Mohan <mohan@stackbox.xyz>
2023-06-06 16:35:00 +0530

    feat (exercises 3): :sparkles: implement web server prefix matching
    [https://google.github.io/comprehensive-rust/exercises/day-2/strings-iterators.html]

--- src/exercises/mod.rs
+++ src/exercises/mod.rs
@@ -2,3 +2,4 @@ pub mod health_stats;
 pub mod library;
 pub mod luhn_algorithm;
 pub mod points_polygons;
+pub mod prefix_match;
---
+++ src/exercises/prefix_match.rs
@@ -0,0 +1,50 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut prefix_it = prefix.split("/");
+    let mut request_path_it = request_path.split("/");
+
+    while let Some(sub_path) = prefix_it.next() {
+        match request_path_it.next() {
+            Some(r_sub_path) if sub_path.eq(r_sub_path) | sub_path.eq("*") => {
+                println!("matched so far {sub_path} == {r_sub_path}")
+            }
+            _ => return false,
+        }
+    }
+    return true;
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
